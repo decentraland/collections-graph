1c4cbac95e77dc54e88e7219d0fea8c86a6da5ba
Ignacio Mazzara <nachomazzara@gmail.com>
2021-03-09 13:21:45 -0300

    fix: images

--- src/modules/item/index.ts
+++ src/modules/item/index.ts
@@ -5,10 +5,10 @@ export function getItemId(contractAddress: string, itemId: string): string {
 }
 
 export function getItemImage(item: Item): string {
-  let URI = item.URI.split('collections/')  // https://peer.decentraland.[org|zone]/collections/standard/...
+  let URI = item.URI.split('standard/')  // https://peer.decentraland.[org|zone]/collections/standard/...
   let baseURI = URI[0]  // https://peer.decentraland.[org|zone]/
 
-  return baseURI + 'contents/' + item.urn
+  return baseURI + 'contents/' + item.urn + '/image'
 }
 
 export function removeItemMinter(item: Item, minter: string): Array<string> {
