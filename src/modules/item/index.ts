8e42faa8878a33660840e5490c45aa55ddff04ce
Ignacio Mazzara <nachomazzara@gmail.com>
2021-04-16 20:39:11 -0300

    fix: use thumbnails

--- src/modules/item/index.ts
+++ src/modules/item/index.ts
@@ -8,7 +8,7 @@ export function getItemImage(item: Item): string {
   let URI = item.URI.split('standard/')  // https://peer.decentraland.[org|zone]/collections/standard/...
   let baseURI = URI[0]  // https://peer.decentraland.[org|zone]/collections
 
-  return baseURI + 'contents/' + item.urn + '/image'
+  return baseURI + 'contents/' + item.urn + '/thumbnail'
 }
 
 export function removeItemMinter(item: Item, minter: string): Array<string> {
