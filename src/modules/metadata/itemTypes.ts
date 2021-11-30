203743d3badcd9be9fa4b535b8365286d68dc75b
Ignacio Mazzara <nachomazzara@gmail.com>
2021-11-30 17:32:23 -0300

    feat: change to v1

--- src/modules/metadata/itemTypes.ts
+++ src/modules/metadata/itemTypes.ts
@@ -3,7 +3,7 @@ export const UNDEFINED = 'undefined'
 export const WEARABLE_V1 = 'wearable_v1'
 export const WEARABLE_V2 = 'wearable_v2'
 
-export const SMART_WEARABLE_V2 = 'smart_wearable_v2'
+export const SMART_WEARABLE_V1 = 'smart_wearable_v1'
 
 export const WEARABLE_TYPE_SHORT = 'w'
 export const SMART_WEARABLE_TYPE_SHORT = 'sw'
