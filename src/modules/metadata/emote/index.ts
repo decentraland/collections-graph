48033990a40644c6c857621e4896f0834380f106
Juan Cazala <juancazala@gmail.com>
2022-03-11 17:55:29 -0300

    feat: add Emotes

---
+++ src/modules/metadata/emote/index.ts
@@ -0,0 +1,69 @@
+import { log } from '@graphprotocol/graph-ts'
+import { isValidBodyShape } from '..'
+import { Emote, Item, Metadata, NFT, Wearable } from '../../../entities/schema'
+import { toLowerCase } from '../../../utils'
+import { LOOP, SIMPLE } from './categories'
+
+/**
+ * @dev The item's rawMetadata for emotes should follow: version:item_type:name:description:category:bodyshapes
+ * @param item
+ */
+export function buildEmoteItem(item: Item): Wearable | null {
+  let id = item.id
+  let data = item.rawMetadata.split(':')
+  if ((data.length == 6 || data.length == 8) && isValidEmoteCategory(data[4]) && isValidBodyShape(data[5].split(','))) {
+    let emote = Emote.load(id)
+
+    if (emote == null) {
+      emote = new Emote(id)
+    }
+
+    emote.collection = item.collection
+    emote.name = data[2]
+    emote.description = data[3]
+    emote.rarity = item.rarity
+    emote.category = data[4]
+    emote.bodyShapes = data[5].split(',') // Could be more than one
+    emote.save()
+
+    return emote
+  }
+
+  return null
+}
+
+function isValidEmoteCategory(category: string): boolean {
+  if (category == SIMPLE || category == LOOP) {
+    return true
+  }
+
+  log.error('Invalid Category {}', [category])
+
+  return false
+}
+
+export function setItemEmoteSearchFields(item: Item): Item {
+  let metadata = Metadata.load(item.metadata)
+  let emote = Emote.load(metadata.emote)
+
+  item.searchText = toLowerCase(emote.name + ' ' + emote.description)
+  item.searchItemType = item.itemType
+  item.searchEmoteCategory = emote.category
+  item.searchEmoteBodyShapes = emote.bodyShapes
+  item.searchEmoteRarity = emote.rarity
+
+  return item
+}
+
+export function setNFTEmoteSearchFields(nft: NFT): NFT {
+  let metadata = Metadata.load(nft.metadata)
+  let emote = Wearable.load(metadata.emote)
+
+  nft.searchText = toLowerCase(emote.name + ' ' + emote.description)
+  nft.searchItemType = nft.itemType
+  nft.searchEmoteCategory = emote.category
+  nft.searchEmoteBodyShapes = emote.bodyShapes
+  nft.searchEmoteRarity = emote.rarity
+
+  return nft
+}
--- src/modules/metadata/index.ts
+++ src/modules/metadata/index.ts
@@ -1,12 +1,9 @@
 import * as itemTypes from './itemTypes'
 import { Item, NFT, Metadata } from '../../entities/schema'
-import {
-  setNFTWearableSearchFields,
-  setItemWearableSearchFields,
-  buildWearableItem,
-  buildWearableV1
-} from './wearable'
+import { setNFTWearableSearchFields, setItemWearableSearchFields, buildWearableItem, buildWearableV1 } from './wearable'
 import { Wearable as WearableRepresentation } from '../../data/wearablesV1'
+import { buildEmoteItem, setItemEmoteSearchFields, setNFTEmoteSearchFields } from './emote'
+import { log } from '@graphprotocol/graph-ts'
 
 /**
  * @notice the item's metadata must follow: version:item_type:representation_id:data
@@ -23,15 +20,28 @@ export function buildItemMetadata(item: Item): Metadata {
   if (data.length >= 2) {
     let type = data[1]
 
-    let wearable = buildWearableItem(item)
-    if (wearable != null && type == itemTypes.WEARABLE_TYPE_SHORT) {
-      metadata.itemType = itemTypes.WEARABLE_V2
-      metadata.wearable = wearable.id
-    } else if (wearable != null && type == itemTypes.SMART_WEARABLE_TYPE_SHORT) {
-      metadata.itemType = itemTypes.SMART_WEARABLE_V1
-      metadata.wearable = wearable.id
-    } else {
-      metadata.itemType = itemTypes.UNDEFINED
+    switch (type) {
+      case itemTypes.WEARABLE_TYPE_SHORT: {
+        let wearable = buildWearableItem(item)
+        metadata.itemType = itemTypes.WEARABLE_V2
+        metadata.wearable = wearable.id
+        break
+      }
+      case itemTypes.SMART_WEARABLE_TYPE_SHORT: {
+        let wearable = buildWearableItem(item)
+        metadata.itemType = itemTypes.SMART_WEARABLE_V1
+        metadata.wearable = wearable.id
+        break
+      }
+      case itemTypes.EMOTE_TYPE_SHORT: {
+        let emote = buildEmoteItem(item)
+        metadata.itemType = itemTypes.EMOTE_V1
+        metadata.emote = emote.id
+        break
+      }
+      default: {
+        metadata.itemType = itemTypes.UNDEFINED
+      }
     }
   } else {
     metadata.itemType = itemTypes.UNDEFINED
@@ -41,7 +51,6 @@ export function buildItemMetadata(item: Item): Metadata {
 
   return metadata!
 }
-
 
 export function buildWearableV1Metadata(item: Item, representation: WearableRepresentation): Metadata {
   let metadata = new Metadata(representation.id)
@@ -56,27 +65,37 @@ export function buildWearableV1Metadata(item: Item, representation: WearableRepr
   return metadata!
 }
 
-
 export function setItemSearchFields(item: Item): Item {
-  if (
-    item.itemType == itemTypes.WEARABLE_V2 ||
-    item.itemType == itemTypes.WEARABLE_V1 ||
-    item.itemType == itemTypes.SMART_WEARABLE_V1
-  ) {
+  if (item.itemType == itemTypes.WEARABLE_V2 || item.itemType == itemTypes.WEARABLE_V1 || item.itemType == itemTypes.SMART_WEARABLE_V1) {
     return setItemWearableSearchFields(item)
+  }
+  if (item.itemType == itemTypes.EMOTE_V1) {
+    return setItemEmoteSearchFields(item)
   }
 
   return item
 }
 
 export function setNFTSearchFields(nft: NFT): NFT {
-  if (
-    nft.itemType == itemTypes.WEARABLE_V2 ||
-    nft.itemType == itemTypes.WEARABLE_V1 ||
-    nft.itemType == itemTypes.SMART_WEARABLE_V1
-  ) {
+  if (nft.itemType == itemTypes.WEARABLE_V2 || nft.itemType == itemTypes.WEARABLE_V1 || nft.itemType == itemTypes.SMART_WEARABLE_V1) {
     return setNFTWearableSearchFields(nft)
   }
 
+  if (nft.itemType == itemTypes.EMOTE_V1) {
+    return setNFTEmoteSearchFields(nft)
+  }
+
   return nft
+}
+
+export function isValidBodyShape(bodyShapes: string[]): boolean {
+  for (let i = 0; i++; i < bodyShapes.length) {
+    let bodyShape = bodyShapes[i]
+    if (bodyShape != 'BaseFemale' && bodyShape != 'BaseMale') {
+      log.error('Invalid BodyShape {}', [bodyShape])
+      return false
+    }
+  }
+
+  return true
 }
