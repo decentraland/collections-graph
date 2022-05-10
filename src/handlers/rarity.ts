c73b884db2df0efad0cee56621db9b998dfc1781
Nando <24811313+fzavalia@users.noreply.github.com>
2022-05-10 12:11:16 -0300

    feat: Checks

--- src/handlers/rarity.ts
+++ src/handlers/rarity.ts
@@ -5,7 +5,14 @@ import { Rarity } from '../entities/schema'
 export function handleAddRarity(event: AddRarity): void {
   let name = event.params._rarity.name
 
-  let rarity = new Rarity(name)
+  let rarity = Rarity.load(name)
+
+  if (rarity == null) {
+    rarity = new Rarity(name)
+  } else if (rarity.currency === 'USD') {
+    log.warning('Rarity with name {} already updated by new contract', [name])
+    return
+  }
 
   rarity.name = name
   rarity.price = event.params._rarity.price
@@ -16,12 +23,14 @@ export function handleAddRarity(event: AddRarity): void {
 }
 
 export function handleUpdatePrice(event: UpdatePrice): void {
-  let rarity = Rarity.load(event.params._name)
+  let name = event.params._name
+
+  let rarity = Rarity.load(name)
 
   // If currency is USD, it's because it has already been handled by the new RaritiesWithOracle.
   // If so, We don't care about changes made on the on the unused Rarity contract.
   if (rarity.currency == 'USD') {
-    log.warning("Updates made in the old contract will not be reflected in the graph", [])
+    log.warning('Rarity with name {} already updated by new contract', [name])
     return
   }
 
