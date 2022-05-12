980f8aec0bfb3866e51965a6522575149ec4a7ac
Nando <24811313+fzavalia@users.noreply.github.com>
2022-05-12 12:28:29 -0300

    Revert "Revert "feat(raritiesWithOracle): Handle new RaritiesWithOracle contract (#45)" (#50)"
    This reverts commit acbad686f55edfebdee0315133ac1bd6ceecac8b.

---
+++ src/handlers/raritiesWithOracle.ts
@@ -0,0 +1,27 @@
+import { AddRarity, UpdatePrice } from '../entities/RaritiesWithOracle/RaritiesWithOracle'
+import { Rarity } from '../entities/schema'
+
+export function handleAddRarity(event: AddRarity): void {
+  let name = event.params._rarity.name
+
+  let rarity = Rarity.load(name)
+
+  if (rarity == null) {
+    rarity = new Rarity(name)
+  }
+
+  rarity.name = name
+  rarity.price = event.params._rarity.price
+  rarity.maxSupply = event.params._rarity.maxSupply
+  rarity.currency = 'USD'
+
+  rarity.save()
+}
+
+export function handleUpdatePrice(event: UpdatePrice): void {
+  let rarity = Rarity.load(event.params._name)
+
+  rarity.price = event.params._price
+
+  rarity.save()
+}
--- src/handlers/rarity.ts
+++ src/handlers/rarity.ts
@@ -1,21 +1,45 @@
+import { log } from '@graphprotocol/graph-ts'
 import { AddRarity, UpdatePrice } from '../entities/Rarity/Rarity'
 import { Rarity } from '../entities/schema'
 
 export function handleAddRarity(event: AddRarity): void {
-  let rarity = new Rarity(event.params._rarity.name)
+  let name = event.params._rarity.name
 
-  rarity.name = event.params._rarity.name
+  let rarity = Rarity.load(name)
+
+  if (rarity == null) {
+    rarity = new Rarity(name)
+  } else if (rarity.currency === 'USD') {
+    log.warning('Rarity with name {} already updated by new contract', [name])
+    return
+  }
+
+  rarity.name = name
   rarity.price = event.params._rarity.price
   rarity.maxSupply = event.params._rarity.maxSupply
+  rarity.currency = 'MANA'
 
   rarity.save()
 }
 
 export function handleUpdatePrice(event: UpdatePrice): void {
-  let rarity = Rarity.load(event.params._name)
+  let name = event.params._name
 
-  if (rarity != null) {
-    rarity.price = event.params._price
-    rarity.save()
+  let rarity = Rarity.load(name)
+
+  if (rarity == null) {
+    log.warning('Rarity with name {} not found', [name])
+    return
   }
+
+  // If currency is USD, it's because it has already been handled by the new RaritiesWithOracle.
+  // If so, We don't care about changes made on the on the unused Rarity contract.
+  if (rarity.currency == 'USD') {
+    log.warning('Rarity with name {} already updated by new contract', [name])
+    return
+  }
+
+  rarity.price = event.params._price
+
+  rarity.save()
 }
