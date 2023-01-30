235c30c9bb5cf1443e7bd1741bb24b7268dccb5d
Nando <24811313+fzavalia@users.noreply.github.com>
2023-01-30 14:36:48 -0300

    chore: Reformat

--- tests/collection/collection.test.ts
+++ tests/collection/collection.test.ts
@@ -1,8 +1,11 @@
-import { assert, clearStore, test, describe, afterAll, dataSourceMock } from 'matchstick-as/assembly/index'
+import { assert, clearStore, test, describe, afterAll, dataSourceMock, beforeEach } from 'matchstick-as/assembly/index'
 import { Address, BigInt } from '@graphprotocol/graph-ts'
 import { handleSetGlobalMinter } from '../../src/handlers/collection'
 import { addresses, createCollection, createSetGlobalMinterEvent } from './utils'
 import { getStoreAddress } from '../../src/modules/store'
+
+let minter: Address
+let firstListedAt: BigInt | null
 
 describe('collection', () => {
   afterAll(() => {
@@ -10,75 +13,88 @@ describe('collection', () => {
     dataSourceMock.resetValues()
   })
 
-  describe('handleSetGlobalMinter', () => {
-    describe('when the minter is the store', () => {
-      describe('and firstListedAt is null', () => {
-        test('should set firstListedAt timestamp', () => {
-          dataSourceMock.setNetwork('matic')
-
-          let minter = Address.fromString(getStoreAddress())
-          let firstListedAt: BigInt | null = null
-
-          let collectionAddress = addresses[0]
-          let collectionId = collectionAddress.toHexString()
-          let collection = createCollection(collectionId)
-
-          collection.firstListedAt = firstListedAt
-
-          collection.save()
-
-          let setGlobalMinterEvent = createSetGlobalMinterEvent(collectionAddress, minter, true)
-
-          handleSetGlobalMinter(setGlobalMinterEvent)
-
-          assert.fieldEquals('Collection', collectionId, 'firstListedAt', setGlobalMinterEvent.block.timestamp.toString())
-        })
-      })
-
-      describe('and firstListedAt has a value', () => {
-        test('should keep current firstListedAt value', () => {
-          dataSourceMock.setNetwork('matic')
-
-          let minter = Address.fromString(getStoreAddress())
-          let firstListedAt = BigInt.fromI32(100)
-
-          let collectionAddress = addresses[0]
-          let collectionId = collectionAddress.toHexString()
-          let collection = createCollection(collectionId)
-
-          collection.firstListedAt = firstListedAt
-
-          collection.save()
-
-          let setGlobalMinterEvent = createSetGlobalMinterEvent(collectionAddress, minter, true)
-
-          handleSetGlobalMinter(setGlobalMinterEvent)
-
-          assert.fieldEquals('Collection', collectionId, 'firstListedAt', firstListedAt.toString())
-        })
-      })
+  describe('when the network is matic', () => {
+    beforeEach(() => {
+      dataSourceMock.setNetwork('matic')
     })
 
-    describe('when the minter is not the store', () => {
-      test('should keep firstListedAt as null', () => {
-        dataSourceMock.setNetwork('matic')
+    describe('handleSetGlobalMinter', () => {
+      describe('when the minter is the store', () => {
+        beforeEach(() => {
+          minter = Address.fromString(getStoreAddress())
+        })
 
-        let minter = addresses[1]
-        let firstListedAt: BigInt | null = null
+        describe('and firstListedAt is null', () => {
+          beforeEach(() => {
+            firstListedAt = null
+          })
 
-        let collectionAddress = addresses[0]
-        let collectionId = collectionAddress.toHexString()
-        let collection = createCollection(collectionId)
+          test('should set firstListedAt timestamp', () => {
+            let collectionAddress = addresses[0]
+            let collectionId = collectionAddress.toHexString()
+            let collection = createCollection(collectionId)
 
-        collection.firstListedAt = firstListedAt
+            collection.firstListedAt = firstListedAt
 
-        collection.save()
+            collection.save()
 
-        let setGlobalMinterEvent = createSetGlobalMinterEvent(collectionAddress, minter, true)
+            let setGlobalMinterEvent = createSetGlobalMinterEvent(collectionAddress, minter, true)
 
-        handleSetGlobalMinter(setGlobalMinterEvent)
+            handleSetGlobalMinter(setGlobalMinterEvent)
 
-        assert.fieldEquals('Collection', collectionId, 'firstListedAt', 'null')
+            assert.fieldEquals('Collection', collectionId, 'firstListedAt', setGlobalMinterEvent.block.timestamp.toString())
+          })
+        })
+
+        describe('and firstListedAt has a value', () => {
+          beforeEach(() => {
+            firstListedAt = BigInt.fromI32(100)
+          })
+
+          test('should keep current firstListedAt value', () => {
+            let collectionAddress = addresses[0]
+            let collectionId = collectionAddress.toHexString()
+            let collection = createCollection(collectionId)
+
+            collection.firstListedAt = firstListedAt
+
+            collection.save()
+
+            let setGlobalMinterEvent = createSetGlobalMinterEvent(collectionAddress, minter, true)
+
+            handleSetGlobalMinter(setGlobalMinterEvent)
+
+            assert.fieldEquals('Collection', collectionId, 'firstListedAt', (firstListedAt as BigInt).toString())
+          })
+        })
+      })
+
+      describe('when the minter is not the store', () => {
+        beforeEach(() => {
+          minter = addresses[1]
+        })
+
+        describe('and firstListedAt is null', () => {
+          beforeEach(() => {
+            firstListedAt = null
+          })
+
+          test('should keep firstListedAt as null', () => {
+            let collectionAddress = addresses[0]
+            let collectionId = collectionAddress.toHexString()
+            let collection = createCollection(collectionId)
+
+            collection.firstListedAt = firstListedAt
+
+            collection.save()
+
+            let setGlobalMinterEvent = createSetGlobalMinterEvent(collectionAddress, minter, true)
+
+            handleSetGlobalMinter(setGlobalMinterEvent)
+
+            assert.fieldEquals('Collection', collectionId, 'firstListedAt', 'null')
+          })
+        })
       })
     })
   })
