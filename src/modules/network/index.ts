5e86a55cf72b72cf996f69accb864ad7511044e3
Gabriel Diaz <gabrieldiaz31@gmail.com>
2024-04-14 18:29:12 -0300

    fix: Polygon amoy references

--- src/modules/network/index.ts
+++ src/modules/network/index.ts
@@ -2,5 +2,5 @@ import { dataSource } from '@graphprotocol/graph-ts'
 
 export function getNetwork(): string {
   let network = dataSource.network()
-  return network == "mainnet" ? "ethereum" : network
+  return network == "mainnet" ? "ethereum" : network == 'polygon-amoy' ? 'amoy' : network
 }
